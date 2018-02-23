020862f9acfe3206cd20b83045182ab0334cd911
Rob Vanstone <rvanstone@xebialabs.com>
2018-02-23 13:06:36 +0000

    Adding first auto release

---
+++ autoReleaseFiles/octopusAuto.groovy
@@ -0,0 +1,51 @@
+// Exported from:        http://Robs-Pro.local:5516/#/templates/Folder43fbe2f960104407ac113711e83ba31b-Release553d70dc510b4382b96e039dab4399dc/releasefile
+// XL Release version:   7.5.0
+// Date created:         Fri Feb 23 13:02:19 GMT 2018
+
+xlr {
+  release('AutoRelease-GenerateTasks') {
+    variables {
+      mapVariable('components') {
+        showOnReleaseStart false
+        value 'component2':'1.2.1','component3':'1.1.1','component1':'1.0.1'
+      }
+      booleanVariable('isManual') {
+        
+      }
+    }
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-02-08T09:00:00+0000')
+    // no DSL renderer found for property 'riskProfile' of type 'xlrelease.Release'
+    phases {
+      phase('DEV') {
+        color '#009CDB'
+        tasks {
+          userInput('Choose Components') {
+            description 'Please enter the required information below.'
+            owner 'admin'
+            precondition 'releaseVariables[\'isManual\']'
+            variables {
+              variable 'components'
+            }
+          }
+          script('Add tasks to this release') {
+            script 'deployComponents = releaseVariables[\'components\']\n' +
+                   'containerTask = taskApi.searchTasksByTitle(\'OctopusDeploy\',None, getCurrentRelease().id)\n' +
+                   'print (containerTask)\n' +
+                   'if containerTask[0].id:\n' +
+                   '    print "Found the following task %s" % containerTask[0].id\n' +
+                   '    for key, value in deployComponents.iteritems():\n' +
+                   '      task = taskApi.newTask("octopus.DeployRelease")\n' +
+                   '      task.title = "Deploy %s at version %s" % (key,value)\n' +
+                   '      task.pythonScript.environment = "Dev"\n' +
+                   '      taskApi.addTask(containerTask[0].id, task)\n' +
+                   '    \n' +
+                   'print "Done"'
+          }
+          parallelGroup('OctopusDeploy') {
+            
+          }
+        }
+      }
+    }
+  }
+}
