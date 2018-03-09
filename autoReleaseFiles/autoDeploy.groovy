ec2fd7aa200fe816bd58bd2d302a3b79a86db266
Rob Vanstone <rvanstone@xebialabs.com>
2018-03-09 11:24:40 +0000

    Updating version

--- autoReleaseFiles/autoDeploy.groovy
+++ autoReleaseFiles/autoDeploy.groovy
@@ -1,13 +1,11 @@
-// Exported from:        http://Robs-Pro.local:5516/#/templates/Folder5189c55f4221489b97f0726b15373b34-Release553d70dc510b4382b96e039dab4399dc/releasefile
-// XL Release version:   7.5.0
-// Date created:         Fri Feb 23 15:46:08 GMT 2018
+// Date created:         Fri Mar 09 11:17:33 GMT 2018
 
 xlr {
-  release('AutoRelease-GenerateTasks-1.0.3') {
+  release('AutoRelease-GenerateTasks-1.0.4') {
     variables {
       mapVariable('components') {
         showOnReleaseStart false
-        value 'component2':'1.7.1','component3':'1.2.1','component1':'1.0.1'
+        value 'component2':'1.2.1','component3':'1.1.1','component1':'1.0.1'
       }
       booleanVariable('isManual') {
         
@@ -15,10 +13,9 @@ xlr {
     }
     scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-02-08T09:00:00+0000')
     tags 'autoRelease'
-    autoStart 'true'
     scriptUsername 'admin'
     scriptUserPassword '{b64}bc4KjBgfVvxUgRxHVWGNVg=='
-    // no DSL renderer found for property 'riskProfile' of type 'xlrelease.Release'
+    autoStart 'true'
     phases {
       phase('DEV') {
         color '#009CDB'
