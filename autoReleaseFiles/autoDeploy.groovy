a20d5a5c743e9b4236097e794006657298671efc
Rob Vanstone <rvanstone@xebialabs.com>
2018-03-09 10:54:43 +0000

    Updating to latest version

--- autoReleaseFiles/autoDeploy.groovy
+++ autoReleaseFiles/autoDeploy.groovy
@@ -3,7 +3,7 @@
 // Date created:         Fri Feb 23 15:46:08 GMT 2018
 
 xlr {
-  release('AutoRelease-GenerateTasks-1.0.2') {
+  release('AutoRelease-GenerateTasks-1.0.3') {
     variables {
       mapVariable('components') {
         showOnReleaseStart false
